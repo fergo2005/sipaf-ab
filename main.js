e01f87e90dc18fd1f7cadbac8459c4c16665f522
FSGomez <fergo2005@gmail.com>
2025-10-08 13:08:39 -0300

    agregue todo con add . para 1er commit

---
+++ main.js
@@ -0,0 +1,105 @@
+
+
+
+// NOTAS:
+// Para las areas pensar en un segun sea indice 0=A, 1=B, 2=C, 3-D, por ejemplo
+// Objeto evidencia = 4461-00001-2025-MM-DD-Area-Definicion-IdIndicio-LugarLevantamiento-DescriciónMuestra. 
+
+
+const creaAreas= "";
+
+const areasNombre = [];
+console.log("Areas: " + areasNombre);
+
+areasNombre.push("A");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("B");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("Baño");
+console.log("Areas: " + areasNombre.join(" - "));
+
+
+
+
+
+
+
+
+const areasDescripcion = [];
+console.log("Areas: " + areasDescripcion);
+
+areasDescripcion.push("Living");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("Comedor");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("Baño");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+
+
+
+// de otro archivo
+
+
+
+// Objeto evidencia = 4461-00001-2025-MM-DD-Area-Definicion-IdIndicio-LugarLevantamiento-DescriciónMuestra. 
+
+
+const creaAreas= "";
+
+const areasNombre = [];
+console.log("Areas: " + areasNombre);
+
+areasNombre.push("A");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("B");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("");
+console.log("Areas: " + areasNombre.join(" - "));
+
+areasNombre.push("Baño");
+console.log("Areas: " + areasNombre.join(" - "));
+
+
+
+
+
+
+
+
+const areasDescripcion = [];
+console.log("Areas: " + areasDescripcion);
+
+areasDescripcion.push("Living");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("Comedor");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+areasDescripcion.push("Baño");
+console.log("Areas: " + areasDescripcion.join(" - "));
+
+
+
+
+// NOTAS:
+// Para las areas pensar en un segun sea indice 0=A, 1=B, 2=C, 3-D, por ejemplo
+
+
+
+
